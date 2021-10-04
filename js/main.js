fa454419f12d85b9bf17b955021d545aad015dea
Maxim <bmw_077@mail.ru>
2021-10-04 23:42:12 +0300

    vue project

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(function() {
+
+    $(".menu-tab__item").click(function() {
+        $('.menu-tab__item ').not(this).removeClass('active');
+        $(this).toggleClass('active');
+    });
+
+    $(".menu__list-link").click(function() {
+        $('.menu__list-link').not(this).removeClass('menu__list-link--active');
+        $(this).toggleClass('menu__list-link--active');
+    });
+
+    $(".header-btn").on('click', function() {
+        $(".menu").toggleClass('menu--open');
+    });
+})
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: ''
+    },
+    mounted() {
+        axios
+            .get('education.json')
+            .then(response => (this.message = response.data));
+    }
+})
+
+
+
